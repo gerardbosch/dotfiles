faedf5371fe284af29e39f876bd64275930ce3f6
Gerard Bosch <gerard.bosch@gmail.com>
2021-02-08 02:06:52 +0100

    Update bootstrap doc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 # dotfile manager written in bash).
 #
 # First installs (i.e. clones to the expected place) Homeshick. Then, homeshick
-# bootstraps my dotfiles.
+# bootstraps my dotfiles (clone and link).
 
 CASTLES=~/.homesick/repos
 
@@ -18,9 +18,13 @@ echo
 echo
 echo '🎉 Bootstrap finished!'
 echo
-echo "You can check if Zsh is installed running: ${ul}zsh --version${eul}"
-echo 'If its not, you can install it with your package manager, e.g. apt'
+echo "* You can check if Zsh is installed running: ${ul}zsh --version${eul}"
+echo '  If its not, you can install it with your package manager, like APT or Brew.'
 echo
-echo "Then you can change the login shell to Zsh 🚀🚀: ${ul}chsh -s "'$(which zsh)'"${eul}"
-echo 'Log out and log back in to enjoy ❤️  your new shell; or run zsh from within bash.'
+echo '  You may probably want to have a look at this: https://docs.brew.sh/Installation'
 echo
+echo "* Then, you can change the login shell to Zsh 🚀🚀: ${ul}chsh -s "'$(which zsh)'"${eul}"
+echo '  Log out and log back in to enjoy ❤️  your new shell; or run zsh from within bash.'
+echo
+echo '* Finally, you may want to install some essentials, just run (et al.):'
+echo '  ~/.homesick/repos/dotfiles/brew-install-essentials'
