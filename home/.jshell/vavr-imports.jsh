79d0d633747ae0dccfed5cedf61a9cd625e72147
Gerard Bosch <gerard.bosch@gmail.com>
2021-02-08 02:44:53 +0100

    Add more aliases, incl. vavr and .jshell directory

---
+++ home/.jshell/vavr-imports.jsh
@@ -0,0 +1,22 @@
+import java.io.*
+import java.math.*
+import java.net.*
+import java.nio.charset.*
+import java.nio.file.*
+
+// import java.util.*
+// import java.util.concurrent.*
+import java.util.concurrent.TimeUnit
+
+import java.util.function.*
+import java.util.prefs.*
+import java.util.regex.*
+
+// import java.util.stream.*
+
+import io.vavr.*
+import io.vavr.collection.*
+import io.vavr.concurrent.*
+import io.vavr.control.*
+
+import static io.vavr.API.*
--- home/.shell/aliases.sh
+++ home/.shell/aliases.sh
@@ -1,11 +1,20 @@
+# --- Misc ---
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-# Copy: use rsync (it shows a progress bar, and recursion)
+# copy: use rsync (it shows a progress bar, and recursion)
 alias cpv='rsync -ah --info=progress2'
 
+## grep
 alias gp='grep -n --exclude-dir .svn --exclude-dir .git'
+#alias gp='grep -n --color=auto --exclude-dir=".svn" --exclude-dir=".git" --exclude-dir="target"'
+
+alias hs='homeshick'
+alias tree='tree -F --dirsfirst'
+alias cls='clear'
+alias print_colors_shell='for i in {0..255}; do print -Pn "%${i}F${(l:3::0:)i}%f " ${${(M)$((i%8)):#7}:+"\n"}; done'
 
 # Audacious play
 alias p='setsid audacious -e . >/dev/null'
@@ -16,16 +25,17 @@ alias pt='setsid audacious -E . >/dev/null'  # play in templist
 #alias mnt="mount | awk -F' ' '{ printf \"%s\t%s\n\",\$1,\$3; }' | column -t | egrep ^/dev/ | sort"
 alias mnt="mount | awk '$1 ~ /\/dev/ { print $1,$3; }' | column -t | sort"
 
-# Git
+# --- Development ---
+
+## Git
 alias cg='cd $(git rev-parse --show-toplevel)' # cd to the Git root dir
+# copy-branch (cpb) Copy git branch name to clipboard alias (plattform independent)
+#alias cpb='git rev-parse --abbrev-ref HEAD | clipcopy'
 
-# Misc
-alias hs='homeshick'
+## jshell with vavr loaded and imports in place :) ##
+alias vavr='jshell --class-path "$(coursier fetch -p io.vavr:vavr:0.10.2)" --startup ~/.jshell/vavr-imports.jsh'
 
-# Dev
-
-
-# Maven
+## Maven
 alias mcp='mvn clean package'
 alias mcps='mvn clean package -DskipTests -Dcheckstyle.skip'
 alias mcv='mvn clean verify'
