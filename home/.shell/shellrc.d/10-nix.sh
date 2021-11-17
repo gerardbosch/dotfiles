4e9dd187193c288ae548a486efcc5a5ae9412884
Gerard Bosch <gerard.bosch@gmail.com>
2021-11-17 17:35:50 +0100

    Add Nix script sourcing

---
+++ home/.shell/shellrc.d/10-nix.sh
@@ -0,0 +1,5 @@
+#
+# Source Nix package manager: https://nixos.org/manual/nix/stable/introduction.html
+#
+
+[ -e "$HOME"/.nix-profile/etc/profile.d/nix.sh ] && . "$HOME"/.nix-profile/etc/profile.d/nix.sh # added by Nix installer
