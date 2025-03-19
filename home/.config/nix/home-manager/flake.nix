f1ae616f672367f09fba9af6bbd932d8cff3f469
Gerard Bosch <gerard.bosch@gmail.com>
2025-03-19 19:10:15 +0100

    nix: Add nixGL overlay to the home-manager flake (not tested)

--- home/.config/nix/home-manager/flake.nix
+++ home/.config/nix/home-manager/flake.nix
@@ -4,22 +4,36 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixGL.url   = "github:nix-community/nixGL";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, nixGL, home-manager, ... }:
     let
+      # Define an overlay to wrap certain packages with nixGL
+      nixGlOverlay = self: super: {
+        alacritty = nixGL.libGLConfig.wrap super.alacritty;
+        calibre   = nixGL.libGLConfig.wrap super.calibre;
+      };
+
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+
+      #pkgs = nixpkgs.legacyPackages.${system};
+      # Import Nixpkgs with the overlay applied.
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ nixGlOverlay ];
+      };
+
     in {
       homeConfigurations.gerard = home-manager.lib.homeManagerConfiguration {
+
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
+        # Specify your home configuration modules here
         modules = [ ./home.nix ];
 
         # Optionally use extraSpecialArgs
@@ -27,3 +41,4 @@
       };
     };
 }
+
