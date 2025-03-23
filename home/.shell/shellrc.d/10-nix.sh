dd2193231b77784360456d9cb1f6243b8c552488
Gerard Bosch <gerard.bosch@gmail.com>
2025-03-23 22:45:47 +0100

    nix(home-manager): Source shell environment

--- home/.shell/shellrc.d/10-nix.sh
+++ home/.shell/shellrc.d/10-nix.sh
@@ -2,4 +2,9 @@
 # Source Nix package manager: https://nixos.org/manual/nix/stable/introduction.html
 #
 
-[ -e "$HOME"/.nix-profile/etc/profile.d/nix.sh ] && . "$HOME"/.nix-profile/etc/profile.d/nix.sh # added by Nix installer
+# Old Nix single-user installation
+#[ -e "$HOME"/.nix-profile/etc/profile.d/nix.sh ] && . "$HOME"/.nix-profile/etc/profile.d/nix.sh # added by Nix installer
+
+# Home Manager: https://nix-community.github.io/home-manager/index.xhtml#ch-installation
+[ -e "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh" ] && . "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
