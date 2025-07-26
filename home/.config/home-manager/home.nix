88a76ba7874ca2943fd6199be24aefad82d0e005
Gerard Bosch <gerard.bosch@gmail.com>
2025-07-26 03:08:58 +0200

    nix: Structure home.nix packages in categories

--- home/.config/home-manager/home.nix
+++ home/.config/home-manager/home.nix
@@ -1,14 +1,26 @@
 { config, lib, pkgs, nixgl, ... }:
 
 let
-  isLinux = pkgs.stdenv.isLinux == "x86_64-linux";
-  isMacOs = pkgs.stdenv.isDarwin == "x86_64-darwin";
+  isLinux = pkgs.stdenv.isLinux;
+  isDarwin = pkgs.stdenv.isDarwin;
 
   # Load additional packages if `local-packages.nix` exists
-  localPackages =
-    if builtins.pathExists ./local-packages.nix
-    then import ./local-packages.nix
+
+
+localPackages =
+  let
+    file = ./local-packages.nix;
+  in
+    if builtins.pathExists file
+    then (import file { inherit pkgs; })
     else [];
+
+  # localPackages =
+    # if builtins.pathExists ./local-packages.nix
+    # then import ./local-packages.nix { inherit pkgs; }
+    # else [];
+
+  # trace "Local packages: ${toString localPackages}"
 
   # Utility function to pin a package version (tag/commit)
   # Needs the integrity hash sha256, use my `sd nix commit-hash-sha256`
@@ -18,6 +30,145 @@ let
     rev = tag;
     sha256 = integritySha256;
   }) { system = builtins.currentSystem; };
+
+
+  cliPackages = with pkgs; [
+    bat
+    bat-extras.batdiff
+    bat-extras.batgrep
+    bat-extras.batman
+    bat-extras.batpipe
+    bat-extras.batwatch
+    bat-extras.prettybat
+    btop
+    chafa
+    cloc
+    cowsay
+    csvkit
+    delta
+    diffr
+    direnv
+    dos2unix
+    duf
+    exiftool
+    eza
+    fasd
+    fd
+    ffmpeg
+    fzf
+    gh
+    git-extras
+    git-filter-repo
+    git-review
+    gitFull
+    gitleaks
+    hugo
+    #jekyll # gave errors: Use `nix-shell -p bundler`
+    jq
+    jumpapp
+    kmonad
+    lesspipe
+    lynx
+    mc
+    mdcat
+    mediainfo
+    ncdu
+    neofetch
+    neovim
+    # nh # Nix Helper CLI # Added in programs
+    nodejs
+    nvimpager
+    pdftk
+    pre-commit
+    ranger
+    rclone
+    ripgrep
+    ripgrep-all
+    shellcheck
+    tealdeer
+    tree
+    xan # Rust cli to process csv files
+    #vdhcoapp
+    yt-dlp
+
+    # -- CloudFlare wrangler ---------------------------------------------------
+    #(pin "wrangler@4.4.0" "sha256-+VRNw/n2jsAm90Jv9gkFBYHfj+tuEhLwW1KZx9xTfUo=").wrangler # CloudFlare workers CLI
+
+    # pin wrangler
+    # (import (pkgs.fetchFromGitHub {
+    #   owner = "cloudflare";
+    #   repo = "workers-sdk";
+    #   rev = "wrangler@4.4.0"; # e.g. commit with wrangler 3.79.0
+    #   sha256 = "sha256-+VRNw/n2jsAm90Jv9gkFBYHfj+tuEhLwW1KZx9xTfUo=";
+    # }) { system = builtins.currentSystem; }).wrangler
+
+    # (pkgs.wrangler.override {
+    #   dontCheckForBrokenSymlinks = true;
+    # })
+
+    # TODO
+    #wrangler
+
+    # TODO Broken package, this is a temporary workaround until fixed
+    # (pkgs.wrangler.overrideAttrs (oldAttrs: {
+    #   dontCheckForBrokenSymlinks = true;
+    # }))
+    # --------------------------------------------------------------------------
+  ];
+
+  graphicalPackages = with pkgs; [
+    activitywatch
+    blanket
+    caffeine-ng
+    copyq
+    firefox
+    gimp
+    #jetbrains-toolbox # 2025-07 It's failing: no xdg .desktop/launcher, no IDE updates,...
+    ksnip
+    obsidian
+    slack
+    vlc
+    workrave
+  ];
+
+  devtoolsPackages = with pkgs; [
+    #ammonite
+    bruno # local git-based postman replacement
+    jwt-cli
+    #nodePackages.localtunnel
+    #nushell
+    #scala-cli
+    #pipx
+    #postman
+    go   # required-by: hugo
+    #github:ceedubs/unison-nix#packages.x86_64-linux.ucm
+    vscode
+    #warp-terminal
+    waveterm
+    #yarn
+  ];
+
+  enterprisePackages = with pkgs; [
+    awscli2
+    #aws-sso-cli
+    kubectl # krew plugin manager not included
+    kubelogin-oidc
+    slack
+  ];
+
+  linuxPackages = with pkgs; [
+    (config.lib.nixGL.wrap alacritty)
+    (config.lib.nixGL.wrap calibre)
+    emote
+    livecaptions
+    postman
+    remmina
+    #(config.lib.nixGL.wrap ulauncher) # problems, install with yay
+    yad
+  ];
+
+  macosPackages = with pkgs; [
+  ];
 
 in
 {
@@ -61,152 +212,13 @@ in
   # === Packages ===
   # ----------------
 
-  home.packages = with pkgs; [
-
-    # === CLI ===
-
-    # nh # Nix Helper CLI # Added in programs
-    lesspipe
-    nvimpager
-    fasd
-    fd
-    fzf
-    gitFull
-    git-extras
-    git-review
-    ranger
-    cloc
-    hugo
-    ncdu
-    mc
-    duf
-    eza
-    bat
-    bat-extras.batdiff
-    bat-extras.batgrep
-    bat-extras.batman
-    bat-extras.batpipe
-    bat-extras.batwatch
-    bat-extras.prettybat
-    diffr
-    delta
-    ripgrep
-    ripgrep-all
-    neovim
-    tree
-    cowsay
-    jumpapp
-    kmonad
-    ffmpeg
-    tealdeer
-    gh
-    #fd # added in programs
-    gitleaks
-    pre-commit
-    chafa
-    exiftool
-    csvkit
-    xan # Rust cli to process csv files
-    mdcat
-    shellcheck
-    lynx
-    dos2unix
-    rclone
-    git-filter-repo
-    yt-dlp
-    mediainfo
-    nodejs
-    direnv
-    btop
-    jq
-    jwt-cli
-    neofetch
-    pdftk
-    #warp-terminal
-    waveterm
-
-    # -- CloudFlare wrangler ---------------------------------------------------
-    #(pin "wrangler@4.4.0" "sha256-+VRNw/n2jsAm90Jv9gkFBYHfj+tuEhLwW1KZx9xTfUo=").wrangler # CloudFlare workers CLI
-
-    # pin wrangler
-    # (import (pkgs.fetchFromGitHub {
-    #   owner = "cloudflare";
-    #   repo = "workers-sdk";
-    #   rev = "wrangler@4.4.0"; # e.g. commit with wrangler 3.79.0
-    #   sha256 = "sha256-+VRNw/n2jsAm90Jv9gkFBYHfj+tuEhLwW1KZx9xTfUo=";
-    # }) { system = builtins.currentSystem; }).wrangler
-
-    # (pkgs.wrangler.override {
-    #   dontCheckForBrokenSymlinks = true;
-    # })
-
-    # TODO
-    #wrangler
-
-    # TODO Broken package, this is a temporary workaround until fixed
-    # (pkgs.wrangler.overrideAttrs (oldAttrs: {
-    #   dontCheckForBrokenSymlinks = true;
-    # }))
-    # --------------------------------------------------------------------------
-
-    #github:ceedubs/unison-nix#packages.x86_64-linux.ucm
-    #nodePackages.localtunnel
-
-    #yarn
-    #pipx
-    #nushell
-    #scala-cli
-    #vdhcoapp
-    #ammonite
-
-    # -- Other
-    go   # required-by: hugo
-    #jekyll # gave errors: Use `nix-shell -p bundler`
-
-    # === Graphical ===
-    activitywatch
-    blanket
-    copyq
-    firefox
-    #jetbrains-toolbox # 2025-07 It's failing: no xdg .desktop/launcher, no IDE updates,...
-    ksnip
-    obsidian
-    vlc
-    vscode
-    workrave
-
-    # == TODO Must be in Linux!
-    (config.lib.nixGL.wrap alacritty)
-    caffeine-ng
-    emote
-    gimp
-    livecaptions
-    #(config.lib.nixGL.wrap ulauncher) # problems, install with yay
-    yad
-
-    # == TODO Move to localPackages
-    bruno # local git-based postman replacement
-    (config.lib.nixGL.wrap calibre)
-    postman
-    remmina
-    slack
-
-    # == TODO Enterprise: Create a group (or similar thing) for enterprise or `enterprise-aws`, `enterprise-essential`, enterprise-foo
-    #     that can be enabled somehow with a local file with the groups to enable/install =)
-    #     think on which groups could exist for specific apps like aws
-    awscli2
-    #aws-sso-cli
-    kubectl # krew plugin manager not included
-    kubelogin-oidc
-
-  ] ++ (if isLinux then [
-
-    # Linux specific
-
-  ] else if isMacOs then [
-
-    # macOS specific
-
-  ] else []) ++ localPackages;
+  home.packages =
+    cliPackages ++
+    graphicalPackages ++
+    devtoolsPackages ++
+    # TODO Conditionally enable this based on active profiles (to be configured)
+    enterprisePackages ++
+    (if isLinux then linuxPackages else if isDarwin then macosPackages else []) ++
+    localPackages;
 }
 
